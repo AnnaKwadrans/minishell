f04a6ef0ca82fda145bbe5809e4e3182cada2690
Kevin Gonzales <kevin_gonzales_93@hotmail.com>
2025-05-13 18:55:37 +0200

    here_doc advance

--- here_doc/here_doc.c
+++ here_doc/here_doc.c
@@ -8,6 +8,7 @@
 #include <signal.h>
 
 char	**ft_full_split(char *s, char c);
+int		ft_strcmp(const char *s1, const char *s2);
 
 volatile sig_atomic_t g_heredoc_interrupted = 0;
 
@@ -35,7 +36,7 @@ void restore_signals(void)
 	signal(SIGQUIT, SIG_DFL);
 }
 
-char *without_last_spaces(char *str)
+char *remove_trailing_newline(char *str)
 {
 	int		len;
 	char	*new_str;
@@ -83,29 +84,37 @@ char **add_buffer(char **buffer, char *line)
 char *get_delimiter(char *line)
 {
 	int i = 0;
+	int start = 0;
 	char *delimiter = NULL;
-	char **temp = NULL;
-
-	temp = ft_full_split(line, ' ');
-	if (temp == NULL)
+	
+	while (line[i] != '\0')
 	{
-		printf("Error: Memory allocation failed\n");
-		return (NULL);
-	}
-	while (temp[i] != NULL)
-	{
-		if (ft_strncmp(temp[i], "<<", 2) == 0 && temp[i + 1] != NULL)
+		if (line[i] == '<' && line[i + 1] == '<' && line[i + 2] != '<')
 		{
-			delimiter = ft_strdup(temp[i + 1]);
-			free_array(temp);
-			return (delimiter);
-		}
+				i += 2;
+				while (line[i] == ' ')
+					i++;
+				if (!line[i])
+				{
+					printf("Parse error near `\\n`\n");
+					return (NULL);
+				}
+				start = i;
+				while (line[i] && line[i] != ' ')
+					i++;
+				delimiter = malloc(i - start + 1);
+				if (!delimiter)
+					return (NULL);
+				ft_strlcpy(delimiter, &line[start], i - start + 1);
+				delimiter[i - start] = '\0';
+				return (delimiter);
+			}
 		i++;
 	}
-	return (delimiter);
+	return (NULL);
 }
 
-void	here_doc_mode(char *line)
+char	**here_doc_mode(char *line)
 {
 	char	*delimiter = NULL;
 	char	*new_line = NULL;
@@ -116,11 +125,11 @@ void	here_doc_mode(char *line)
 	printf("<< HERE_DOC MODE RUNNING >>\n");
 	setup_heredoc_signals();
 	delimiter = get_delimiter(line);
-	if (delimiter == NULL)
+	if (!delimiter || *delimiter == '\0')
 	{
-		printf("Error: No delimiter provided\n");
+		printf("Parse error near `\\n`\n");
 		restore_signals();
-		return;
+	    return (NULL);
 	}
 	if (ft_strncmp(delimiter, "EOF", 4) == 0)
 		printf("delimeter is: EOF\n");
@@ -130,7 +139,7 @@ void	here_doc_mode(char *line)
 	while (1)
 	{
 		fflush(stdout);
-		new_line = without_last_spaces(get_next_line(STDIN_FILENO));
+		new_line = remove_trailing_newline(get_next_line(STDIN_FILENO));
 		if (g_heredoc_interrupted)
 		{
 			printf("<<< HERE_DOC MODE INTERRUPTED\n");
@@ -138,34 +147,31 @@ void	here_doc_mode(char *line)
 			free(delimiter);
 			free_array(buffer);
 			restore_signals();
-			return;
+			return (NULL);
 		}
 		if (!new_line)
 		{
-			printf("warning: heredoc delimited by end-of-file (wanted `%s`)\n", delimiter);
+			printf("EOF found without the delimeter\n");
 			free(delimiter);
 			free_array(buffer);
 			restore_signals();
-			return;
+			return (NULL);
 		}
-		if (ft_strncmp (new_line, delimiter, ft_strlen(delimiter)) == 0)
-		{
-			// printf("search for delimiter in '%s'\n", new_line);
-			free(new_line);
+		if (ft_strcmp (new_line, delimiter) == 0)
 			break;
-		}
 		buffer = add_buffer(buffer, new_line);
+		if (!buffer)
+		{
+			printf("Error: Memory allocation failed\n");
+			free(new_line);
+			free(delimiter);
+			restore_signals();
+			return (NULL);
+		}
 	}
 	restore_signals();
 	printf("<<< HERE_DOC MODE STOPPED\n");
-	while (buffer[i] != NULL)
-	{
-		printf("buffer[%d]: %s\n", i, buffer[i]);
-		i++;
-	}
-	free(new_line);
-	free(delimiter);
-	free_array(buffer);
+	return(buffer);
 }
 	
 int is_here_doc(char *line)
@@ -176,7 +182,7 @@ int is_here_doc(char *line)
 	{
 		if (line[i] == '<')
 		{
-		 	if (line[i + 1] == '<' && line[i + 2] == ' ')
+		 	if (line[i + 1] == '<' && line[i + 2] != '<')
 				return (1);
 			else
 				return (0);
@@ -188,26 +194,117 @@ int is_here_doc(char *line)
 
 int main(void)
 {
-	char *line;
+	char *token; // LO QUE HAY ENTRE PIPES
+	char **buffer = NULL;
+	int i = 0;
 
-	line = readline("test >");
 	while (1)
 	{
-		if ((ft_strncmp(line, "here_doc", 8) == 0) && is_here_doc(line))
+		token = readline("test >");
+		if (is_here_doc(token))
 		{
-			here_doc_mode(line);
+			buffer = here_doc_mode(token);
 			printf("\t !!! here should make execve !!!\n");
+			if (buffer)
+			{
+				while (buffer[i] != NULL)
+				{
+					printf("buffer[%d]: %s\n", i, buffer[i]);
+					i++;
+				}
+			}
 		}
-		else if (ft_strncmp(line, "exit", 4) == 0)
+		else if (ft_strncmp(token, "exit", 4) == 0 || !token)
 		{
 			printf("Exiting...\n");
-			free(line);
+			free(token);
 			return (0);
 		}
-		line = readline("test >");
 	}
 }
 
 
-// FALTARÍA CORREGIR Y HACER QUE HERE_DOC DEVUELVA EL BUFFER. 
-// LUEGO SERÁ ESTE BUFFER LOS QUE SE EJECUTARÁ CON EL CMD AL INICIO DE CADA TOKEN.
+// PENDIENTE:
+
+
+// Detectar si el delimitador está en comillas simples, dobles o sin comillas.
+
+// Si está en comillas simples, no se debe expandir.
+
+	// cat << EOF
+	// Hola $USER
+	// Fecha: `date`
+	// EOF
+
+	// Hola tu_nombre_de_usuario
+	// Fecha: Sun May 11 12:34:56 UTC 2025
+
+	// •	Se expanden variables ($USER) y comandos (`date`).
+	// •	El delimitador EOF puede estar en cualquier lugar mientras esté solo en una línea.
+
+	// kev@MacBook-Air-de-Kevin minishell % cat << EOF
+	// heredoc> hello $USER
+	// heredoc> date: `date`
+	// heredoc> EOF
+	// hello kev
+	// date: martes, 13 de mayo de 2025, 01:32:36 CEST
+
+// Si está en comillas dobles, se debe expandir.
+
+	// cat << "EOF"
+	// Hola $USER
+	// Fecha: `date`
+	// EOF
+
+	// Hola $USER
+	// Fecha: `date`
+
+	// •	No se expande nada.
+	// •	El delimitador "EOF" debe coincidir exactamente con "EOF" 
+	// 	(aunque el shell lo reconoce bien sin comillas al cerrar).
+
+	// kev@MacBook-Air-de-Kevin minishell % cat << "EOF"
+	// heredoc> hello $USER
+	// heredoc> date `date`
+	// heredoc> EOF
+	// hello $USER
+	// date `date`
+
+// Si no está en comillas, se debe expandir.
+
+// 	cat << 'EOF'
+// 	Hola $USER
+// 	Fecha: `date`
+// 	EOF
+
+// 	Hola $USER
+// 	Fecha: `date`
+
+// 	•	Igual que el anterior: nada se expande.
+// 	•	'EOF' también hace que el delimitador sea interpretado literalmente.
+
+
+// 	kev@MacBook-Air-de-Kevin minishell % cat << 'EOF'
+// 	heredoc> hello $USER
+// 	heredoc> date `date`
+// 	heredoc> EOF
+// 	hello $USER
+// 	date `date`
+
+// Entonces una vez obtenido el delimitador real, 
+// deberíamos tener en cuenta si el buffer será expandible.
+// Esto podríamos almacenarlo en una variable para saber si el buffer es expandible o no
+// para luego ejecutar y/o expandirlo en la ejecución.
+
+// ¿USAR UNA ESTRUCTURA PARA ELLO?
+
+// Sí
+
+// typedef struct s_heredoc
+// {
+// 	char	*delimiter;    // delimitador sin comillas
+// 	int		quoted;        // 0 = expandible, 1 = literal (no expandir nada)
+// 	char	**buffer;      // contenido del heredoc
+// }	t_heredoc;
+
+// FALTA CONSIDERAR QUE EXISTIRÁN LÍNEAS VACÍAS EN EL BUFFER
