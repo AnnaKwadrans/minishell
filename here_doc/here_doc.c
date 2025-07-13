e49238e5ee02c137db8f9c6f604f597b37d3f3e0
Kevin Gonzales <kevin_gonzales_93@hotmail.com>
2025-07-13 19:45:14 +0200

    last_statud updated with 130 after SIGINT

--- here_doc/here_doc.c
+++ here_doc/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: kegonza <kegonzal@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 00:40:38 by kegonza           #+#    #+#             */
-/*   Updated: 2025/07/13 18:27:01 by kegonza          ###   ########.fr       */
+/*   Updated: 2025/07/13 18:58:52 by kegonza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ char	*expand_vars(t_data *data_program, char *str, bool rm_quotes, bool free_lin
 char	**ft_full_split(char *s, char c);
 int		ft_strcmp(const char *s1, const char *s2);
 void	rl_replace_line(const char *text, int clear_undo);
-
-volatile sig_atomic_t g_heredoc_interrupted = 0;
 
 //  sig_atomic_t
 // 	•	Es un tipo definido por la biblioteca estándar de C (viene de <signal.h>).
@@ -83,7 +81,7 @@ char	**add_buffer(char **buffer, char *line)
 
 void	here_doc_init(char *line, t_heredoc *here_doc)
 {
-	g_sigint_received = 0; // Reiniciar la señal de interrupción
+	g_signal = 0; // Reiniciar la señal de interrupción
 	setup_heredoc_signals();
 	get_delimiter(line, here_doc);
 	if (!here_doc->delimiter)
@@ -108,7 +106,7 @@ t_heredoc	*here_doc_mode(t_data *data_program, char *line)
 	{
 		write(1, "heredoc > ", 10);
 		new_line = remove_trailing_newline(get_next_line(STDIN_FILENO));
-		if (!new_line || g_sigint_received
+		if (!new_line || g_signal
 			|| ft_strcmp(new_line, here_doc->delimiter) == 0)
 			break ;
 		here_doc->buffer = add_buffer(here_doc->buffer, new_line);
@@ -117,7 +115,7 @@ t_heredoc	*here_doc_mode(t_data *data_program, char *line)
 		free(new_line);
 		new_line = NULL;
 	}
-	if (g_sigint_received)
+	if (g_signal)
 		return (here_doc_error(here_doc, "SIGINT"), NULL);
 	if (here_doc->buffer && here_doc->is_expandable)
 	{
--- signals/sigint.c
+++ signals/sigint.c
@@ -6,7 +6,7 @@
 /*   By: kegonza <kegonzal@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 21:10:53 by kegonza           #+#    #+#             */
-/*   Updated: 2025/06/30 21:29:50 by kegonza          ###   ########.fr       */
+/*   Updated: 2025/07/13 18:56:26 by kegonza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	disable_echoctl(void)
 void	sigint_handler(int sig)
 {
 	(void)sig;
-	g_sigint_received = 1; // Indicar que se ha recibido SIGINT
+	g_signal = sig; // Indicar que se ha recibido SIGINT
 	write(1, "\n", 1);
 	rl_replace_line("", 0); // Limpiar la línea actual
 	rl_on_new_line(); // Mover el cursor a la nueva línea
