6a9ea78993c455b640bf520904a2c2d1871f16ff
Kevin Gonzales <kevin_gonzales_93@hotmail.com>
2025-07-13 20:41:16 +0200

    redirections for heredocs added

--- executor/redirect.c
+++ executor/redirect.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirect.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akwadran <akwadran@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: kegonza <kegonzal@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 17:33:29 by akwadran          #+#    #+#             */
-/*   Updated: 2025/07/12 17:42:50 by akwadran         ###   ########.fr       */
+/*   Updated: 2025/07/13 20:39:58 by kegonza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,13 @@ int	redirect(t_cmd *cmd, int pipes, int *fds, int i)
 
 int	redirect_input(t_cmd *cmd, int pipes, int *fds, int i)
 {
+	if (cmd->heredoc)
+	{
+		if (dup2(cmd->fd_in, STDIN_FILENO) < 0)
+			perror("dup2 heredoc");
+		close(cmd->fd_in);
+		return (0);
+	}
 	if (cmd->infile)
 	{
 		if (i != 0)
