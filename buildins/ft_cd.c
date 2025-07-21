909796aa7c058e67c48ef69ae4d1282cc7bcc4bd
Anna Kwadrans <annakwadrans@MacBook-Air-de-Anna.local>
2025-07-22 01:58:19 +0200

    exit & cd

--- buildins/ft_cd.c
+++ buildins/ft_cd.c
@@ -6,21 +6,24 @@
 /*   By: akwadran <akwadran@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 18:00:35 by akwadran          #+#    #+#             */
-/*   Updated: 2025/07/12 18:01:22 by akwadran         ###   ########.fr       */
+/*   Updated: 2025/07/22 01:56:51 by akwadran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-static void	update_pwd_var(t_data *data)
+static void	update_pwd_vars(t_data *data)
 {
 	t_vars	*pwd;
+	t_vars	*oldpwd;
 	char	path[PATH_MAX];
 
 	if (getcwd(path, PATH_MAX) == NULL)
 		return (perror("getcwd failed"));
 	pwd = search_var(data, "PWD");
-	free(pwd->value);
+	oldpwd = search_var(data, "OLDPWD");
+	free(oldpwd->value);
+	oldpwd->value = pwd->value;
 	pwd->value = ft_strdup(path);
 	return ;
 }
@@ -28,6 +31,7 @@ static void	update_pwd_var(t_data *data)
 int	ft_cd(t_data *data, char **args)
 {
 	t_vars	*home_var;
+	t_vars	*oldpwd_var;
 	char	*home_path;
 
 	if (array_size(args) > 2)
@@ -41,11 +45,17 @@ int	ft_cd(t_data *data, char **args)
 		if (chdir(home_var->value) == -1)
 			return (perror("chdir failed"), 1);
 	}
+	else if (array_size(args) == 1 || ft_strncmp(args[1], "-", 1) == 0)
+	{
+		oldpwd_var = search_var(data, "OLDPWD");
+		if (chdir(oldpwd_var->value) == -1)
+			return (perror("chdir failed"), 1);
+	}
 	else
 	{
 		if (chdir(args[1]) == -1)
 			return (perror("chdir failed"), 1);
 	}
-	update_pwd_var(data);
+	update_pwd_vars(data);
 	return (0);
 }
